1a630c2adff66c20eab9b3154350e1c173ea1ea4
Panmax <jiapan.china@gmail.com>
2019-03-27 23:05:47 +0800

    once first_response all_response

---
+++ src/ch23/until_all_done/all_response_test.go
@@ -0,0 +1,36 @@
+package all_response
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func runTask(id int) string {
+	time.Sleep(10 * time.Millisecond)
+	return fmt.Sprintf("The result is from %d", id)
+}
+
+func AllResponse() string {
+	numberOfRunner := 10
+	ch := make(chan string, numberOfRunner)
+	for i := 0; i < numberOfRunner; i++ {
+		go func(i int) {
+			ret := runTask(i)
+			ch <- ret
+		}(i)
+	}
+	finalRet := ""
+	for j := 0; j < numberOfRunner; j++ {
+		finalRet += <-ch + "\n"
+	}
+	return finalRet
+}
+
+func TestFirstResponse(t *testing.T) {
+	t.Log("Before:", runtime.NumGoroutine())
+	t.Log(AllResponse())
+	time.Sleep(time.Second * 1)
+	t.Log("After:", runtime.NumGoroutine())
+}
